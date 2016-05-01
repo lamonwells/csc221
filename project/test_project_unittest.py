9bccb84f13476d3f610a731b8a7aca22bb8a7c9c
lamonwells <lwells2014@yahoo.com>
2016-05-01 12:26:03 -0500

    Final Project Changes

--- project/test_project_unittest.py
+++ project/test_project_unittest.py
@@ -1,43 +1,47 @@
 import unittest
 
-from year import is_leap_year
-
+from project import is_leap_year
 
 class YearTest(unittest.TestCase):
-    def test_leap_year(self):
-        self.assertTrue(is_leap_year(1996))
 
-    def test_non_leap_year(self):
-        self.assertFalse(is_leap_year(1997))
-
-    def test_non_leap_even_year(self):
-        self.assertFalse(is_leap_year(1998))
+  def test_is_leap_year(self):
+        self.assertTrue(is_leap_year(1996));
+        self.assertFalse(is_leap_year(1997));
+        self.assertFalse(is_leap_year(1998));
 
 
-from dna import to_rna
 
-    def test_complement_guanine_to_cytosine(self):
+
+from project import to_rna
+
+class ProjectTests(unittest.TestCase):
+    def test_transcribes_guanine_to_cytosine(self):
          self.assertEqual('C', to_rna('G'))
+         self.assertEqual('c', to_rna('g'))
 
-    def test_complement_cytosine_to_guanine(self):
-          self.assertEqual('G', to_rna('C'))
+    def test_transcribes_cytosine_to_guanine(self):
+         self.assertEqual('G', to_rna('C'))
+         self.assertEqual('g', to_rna('c'))
 
 
-    def test_complement_thymine_to_adenine(self):
+    def test_transcribes_thymine_to_adenine(self):
          self.assertEqual('A', to_rna('T'))
+         self.assertEqual('a', to_rna('c'))
+
+    def test_transcribes_adenine_to_uracil(self):
+         self.assertEqual('U', to_rna('A'))
+         self.assertEqual('u', to_rna('a'))
+
+    def test_transcribes_all(self):
+         self.assertEqual('GGGG', to_rna('CCCC'))
+         self.assertEqual('gggg', to_rna('cccc'))
 
 
-    def test_complement_adenine_to_uracil(self):
-         self.assertEqual ('U', to_rna('A'))
+from project import rna_count
 
-
-if __name__=='__main__':
-     unittest.main()
-
-
-
-
-
-
-
-
+class PROJECTTests(unittest.TestCase):
+    def test_rna_count(self):
+        self.assertEqual(
+            {'letter': 1},
+            rna_count('letter')
+    )
